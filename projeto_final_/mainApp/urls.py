50c8dd1de85585688912b64d36d9c3eb328c5134
fc53355 <fc53355@alunos.fc.ul.pt>
2021-02-16 19:25:08 +0000

    Templates setup

--- projeto_final_/mainApp/urls.py
+++ projeto_final_/mainApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('navbar', views.index, name='index'),
+    path('', views.home, name='home'),
 ]
--- projeto_final_/mainApp/views.py
+++ projeto_final_/mainApp/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(response):
+    return render(response, "mainApp/base.html", {})
+
+def home(response):
+    return render(response, "mainApp/home.html", {})
