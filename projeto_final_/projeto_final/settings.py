6443fe404c4741aa65e5a8baf2da29071b858aeb
jRibeir0 <fc53331@alunos.fc.ul.pt>
2021-02-16 16:15:42 +0000

    db setup

--- projeto_final_/projeto_final/settings.py
+++ projeto_final_/projeto_final/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'projeto_final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME' : 'proj_final',
+	'HOST': '127.0.0.1',
+	'PORT': '3306',
+	'USER': 'root',
+	'PASSWORD': '',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Lisbon'
 
 USE_I18N = True
 
