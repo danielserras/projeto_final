be7fdbf1e67835218ee9364cab5e0b9bd0871829
fc53326 <fc53326@alunos.fc.ul.pt>
2021-02-18 20:11:18 +0000

    template login, pequenas alteraçoes na views e na urls

--- projeto_final_/mainApp/urls.py
+++ projeto_final_/mainApp/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login', views.login_view, name='login_view'),
+    path('register', views.register_view, name='register_view'),
     path('profile', views.profile, name = 'profile'),
     path('startsAgreement', views.startsAgreement, name='startsAgreement'),
     path('', views.index, name='index'),
