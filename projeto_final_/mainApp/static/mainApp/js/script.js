0574dbfe05f0d0245907efa60eba6ceebdc48c4b
fc53355 <fc53355@alunos.fc.ul.pt>
2021-02-19 22:43:02 +0000

    Input fields added to the search page

--- projeto_final_/mainApp/static/mainApp/js/script.js
+++ projeto_final_/mainApp/static/mainApp/js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     //agreement
-    $("#agreementAccept").click(function(){
-        if($(this).prop("checked") == true){
+    $("#agreementAccept").click(function() {
+        if ($(this).prop("checked") == true) {
             $("#agreementSubmit").prop("disabled", false);
-        }
-        else if($(this).prop("checked") == false){
+        } else if ($(this).prop("checked") == false) {
             $("#agreementSubmit").prop("disabled", true);
         }
     })
-
+    document.querySelector('.advancedSearchFormButton').addEventListener('click', function() {
+        this.classList.toggle('rotated');
+    });
 });
