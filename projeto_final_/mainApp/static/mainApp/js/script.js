af05118501bb6830d39357a8562e2bdf89c5b53d
fc53355 <fc53355@alunos.fc.ul.pt>
2021-02-20 23:21:19 +0000

    Finished advanced search template

--- projeto_final_/mainApp/static/mainApp/js/script.js
+++ projeto_final_/mainApp/static/mainApp/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    //Hide advanced search
+    $('#advancedSearchDiv').toggle('invisible');
     //agreement
     $("#agreementAccept").click(function() {
         if ($(this).prop("checked") == true) {
@@ -6,8 +8,19 @@ $(document).ready(function() {
         } else if ($(this).prop("checked") == false) {
             $("#agreementSubmit").prop("disabled", true);
         }
-    })
+    });
+    //Show/Hide advanced search at button press
     document.querySelector('.advancedSearchFormButton').addEventListener('click', function() {
+        $('#advancedSearchDiv').toggle('invisible');
         this.classList.toggle('rotated');
+
+        /*divElem = document.getElementById("advancedSearchDiv");
+        //divElem.style.visibility = "hidden";
+        console.log(divElem.style.visibility);
+        if (divElem.style.visibility === "hidden") {
+            divElem.style.visiblity = "visible";
+        } else {
+            divElem.style.visiblity = "hidden";
+        }*/
     });
 });
