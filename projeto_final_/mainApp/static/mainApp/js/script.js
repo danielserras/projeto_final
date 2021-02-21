8dc58dfad492799fc2c22cc3fd987ff99d0a9b82
fc53355 <fc53355@alunos.fc.ul.pt>
2021-02-21 18:19:13 +0000

    Range bar for prices done. Small bug when the prices are on the far right side

--- projeto_final_/mainApp/static/mainApp/js/script.js
+++ projeto_final_/mainApp/static/mainApp/js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    //Hide advanced search
-    $('#advancedSearchDiv').toggle('invisible');
     //agreement
     $("#agreementAccept").click(function() {
         if ($(this).prop("checked") == true) {
@@ -11,16 +9,43 @@ $(document).ready(function() {
     });
     //Show/Hide advanced search at button press
     document.querySelector('.advancedSearchFormButton').addEventListener('click', function() {
-        $('#advancedSearchDiv').toggle('invisible');
-        this.classList.toggle('rotated');
-
-        /*divElem = document.getElementById("advancedSearchDiv");
-        //divElem.style.visibility = "hidden";
-        console.log(divElem.style.visibility);
-        if (divElem.style.visibility === "hidden") {
-            divElem.style.visiblity = "visible";
+        console.log($("#advancedSearchDiv").css('display'));
+        if ($("#advancedSearchDiv").css('display') === 'none') {
+            console.log('hello');
+            $("#advancedSearchDiv").css('display', 'block');
+            console.log($("#advancedSearchDiv").css('display'));
         } else {
-            divElem.style.visiblity = "hidden";
-        }*/
+            $("#advancedSearchDiv").css('display', 'none');
+        }
+
+        this.classList.toggle('rotated');
+    });
+
+    var v = [550, 1500];
+    $("#slider").slider({
+        range: true,
+        min: 0,
+        max: 2500,
+        values: v,
+        slide: function(event, ui) {
+            if ((ui.values[1] - ui.values[0]) < 150) {
+                $("#labelMin").css('display', 'none');
+                $("#labelMax").css('display', 'none');
+                $("#labelMinToMax").css('display', 'block');
+                $("#labelMinToMax").css('left', ((ui.values[0] + ui.values[1]) / 50) - 4 + "%").text(ui.values[0] + '€ até ' + ui.values[1] + '€');
+            } else {
+                $("#labelMin").css('display', 'block');
+                $("#labelMax").css('display', 'block');
+                $("#labelMinToMax").css('display', 'none');
+                $("#labelMin").css('left', ui.values[0] / 25 + "%").text(ui.values[0] + '€');
+                $("#labelMax").css('left', ui.values[1] / 25 + "%").text(ui.values[1] + '€');
+            }
+        },
+        create: function(event, ui) {
+            $("#labelMin").css('left', v[0] / 25 + "%").text(v[0] + '€');
+            $("#labelMax").css('left', v[1] / 25 + "%").text(v[1] + '€');
+            $("#labelMinToMax").css('left', ((ui.values[0] + ui.values[1]) / 50) - 4 + "%").text(ui.values[0] + '€ até ' + ui.values[1] + '€');
+            $("#labelMinToMax").css('display', 'none');
+        }
     });
 });
