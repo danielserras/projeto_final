d4dff91e62afd4734e80ee43d9346f583ced2f34
fc53355 <fc53355@alunos.fc.ul.pt>
2021-02-23 22:34:42 +0000

    Added ruler to range bar. Rescaled the range bar to 2000 max. Rotated the dropdown of advanced search 180deg

--- projeto_final_/mainApp/static/mainApp/js/script.js
+++ projeto_final_/mainApp/static/mainApp/js/script.js
@@ -21,8 +21,9 @@ $(document).ready(function() {
         this.classList.toggle('rotated');
     });
     //Range bar for rent. Search page
-    var v = [550, 1500];
+    var v = [550, 1200];
     var numTooClose = 150;
+    var r = 20;
     if ($(window).width() < 960) {
         var numTooClose = 200;
     }
@@ -32,26 +33,32 @@ $(document).ready(function() {
     $("#slider").slider({
         range: true,
         min: 0,
-        max: 2500,
+        max: 2000,
         values: v,
         slide: function(event, ui) {
             if ((ui.values[1] - ui.values[0]) < numTooClose) {
                 $("#labelMin").css('display', 'none');
                 $("#labelMax").css('display', 'none');
                 $("#labelMinToMax").css('display', 'block');
-                $("#labelMinToMax").css('left', ((ui.values[0] + ui.values[1]) / 50) - 4.5 + "%").text(ui.values[0] + '€ até ' + ui.values[1] + '€');
+                $("#labelMinToMax").css('left', ((ui.values[0] + ui.values[1]) / (r * 2)) - 4.5 + "%").text(ui.values[0] + '€ até ' + ui.values[1] + '€');
+            } else if (ui.values[1] == 2000) {
+                $("#labelMin").css('display', 'block');
+                $("#labelMax").css('display', 'block');
+                $("#labelMinToMax").css('display', 'none');
+                $("#labelMin").css('left', (ui.values[0] / r) + "%").text(ui.values[0] + '€');
+                $("#labelMax").css('left', (ui.values[1] / r) + "%").text(ui.values[1] + '€ +');
             } else {
                 $("#labelMin").css('display', 'block');
                 $("#labelMax").css('display', 'block');
                 $("#labelMinToMax").css('display', 'none');
-                $("#labelMin").css('left', (ui.values[0] / 25) + "%").text(ui.values[0] + '€');
-                $("#labelMax").css('left', (ui.values[1] / 25) + "%").text(ui.values[1] + '€');
+                $("#labelMin").css('left', (ui.values[0] / r) + "%").text(ui.values[0] + '€');
+                $("#labelMax").css('left', (ui.values[1] / r) + "%").text(ui.values[1] + '€');
             }
         },
         create: function(event, ui) {
-            $("#labelMin").css('left', v[0] / 25 + "%").text(v[0] + '€');
-            $("#labelMax").css('left', v[1] / 25 + "%").text(v[1] + '€');
-            $("#labelMinToMax").css('left', ((ui.values[0] + ui.values[1]) / 50) - 4 + "%").text(ui.values[0] + '€ até ' + ui.values[1] + '€');
+            $("#labelMin").css('left', v[0] / r + "%").text(v[0] + '€');
+            $("#labelMax").css('left', v[1] / r + "%").text(v[1] + '€');
+            $("#labelMinToMax").css('left', ((ui.values[0] + ui.values[1]) / (r * 2)) - 4 + "%").text(ui.values[0] + '€ até ' + ui.values[1] + '€');
             $("#labelMinToMax").css('display', 'none');
         }
     });
