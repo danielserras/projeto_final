735ec52a279f46208ba2cf51e7472054aa2ba285
danielserras <dfserras@gmail.com>
2021-03-20 23:13:14 +0000

    Translations

--- projeto_final_/projeto_final/settings.py
+++ projeto_final_/projeto_final/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt'
 
 TIME_ZONE = 'Europe/Lisbon'
 
@@ -131,6 +131,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [     os.path.join(BASE_DIR, 'locale') ]
 
 
 # Static files (CSS, JavaScript, Images)
