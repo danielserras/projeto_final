8657070cfa618e2440536fa3b5e85ee92cf985e1
danibras <danifjbras11@gmail.com>
2021-04-03 17:31:02 +0100

    Added animation and translation to step progress bar

--- projeto_final_/mainApp/static/mainApp/js/stepProgressBar.js
+++ projeto_final_/mainApp/static/mainApp/js/stepProgressBar.js
@@ -8,21 +8,35 @@ $(document).ready(function(){
     const numSteps = $("#numberOfSteps").html();
     const stepLabels = getStepLabels();
     const completedSteps = $("#numberOfCompletedSteps").html();
+    var calcDistance = (100/(numSteps-1))*(completedSteps-1);
     
     if(numSteps == 5){
         measuresArray = fiveStepsMeasuresArray;
     } else{
         measuresArray = sixStepsMeasuresArray;
     };
-    for (let i = 0; i < measuresArray.length; i++) {
-        $("#stepBarDiv").append("<p class='stepLabel' style='left:"+ measuresArray[i]+"%;'>"+stepLabels[i]+"</p>");
-    }
-    for (let i = 0; i < completedSteps; i++) {
+    if(completedSteps == 1){
         $("#stepBarDiv").append("<div class='completedStep' style='left:"+ stepDistancing +"%;'></div>");
         stepDistancing += 100/(numSteps-1);
     }
-    console.log(stepDistancing - (100/(numSteps-1)));
-    $("#stepBarDiv").append("<div class='stepLine' style='width:"+ (stepDistancing - (100/(numSteps-1))) +"%;'></div>");
+    for (let i = 0; i < measuresArray.length; i++) {
+        $("#stepBarDiv").append("<p class='stepLabel' style='left:"+ measuresArray[i]+"%;'>"+stepLabels[i]+"</p>");
+    }
+    for (let i = 0; i < completedSteps-1; i++) {
+        $("#stepBarDiv").append("<div class='completedStep' style='left:"+ stepDistancing +"%;'></div>");
+        stepDistancing += 100/(numSteps-1);
+    }
+
+    $("#stepBarDiv").append("<div class='stepLine'></div>");
+    $(".stepLine").width((100/(numSteps-1)*(completedSteps-2)+"%"));
+
+    setTimeout(function(){ 
+        $(".stepLine").animate({width: calcDistance+"%"}, 1000);
+    }, 100);
+    setTimeout(function(){ 
+        $("#stepBarDiv").append("<div class='completedStep' style='left:"+ calcDistance +"%;'></div>");
+    }, 1120);
+
     for (let i = 0; i < numSteps - completedSteps; i++) {
         $("#stepBarDiv").append("<div class='emptyStep' style='left:"+ stepDistancing +"%;'></div>");
         stepDistancing += 100/(numSteps-1);
