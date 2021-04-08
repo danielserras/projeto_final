24be1b348812d081a094ea3386682c15ac3b13d6
danielserras <dfserras@gmail.com>
2021-04-09 00:12:34 +0100

    preview of images pretty mutch done

--- projeto_final_/mainApp/static/mainApp/js/editListing.js
+++ projeto_final_/mainApp/static/mainApp/js/editListing.js
@@ -24,14 +24,33 @@ function addimgSlot(slots) {
     let imgFormContainer = $(".imgF");
     let totalForms = $('[id=id_form-TOTAL_FORMS]')[0];
     let imgFormNum = slots-1;
-
-    let newForm = imgFormContainer.children()[5].cloneNode(true);
+    let newForm = imgFormContainer.children()[4].cloneNode(true);
 
     imgFormNum++;
-    newForm.setAttribute('id', 'id_form-'+imgFormNum.toString()+'-images');
-    newForm.setAttribute('name', `form-${imgFormNum.toString()}-images`);
-    newForm.value='';
+    newForm.childNodes[1].innerHTML = '';
+    newForm.childNodes[3].childNodes[1].setAttribute('id', 'id_form-'+imgFormNum.toString()+'-images');
+    newForm.childNodes[3].childNodes[1].setAttribute('name', `form-${imgFormNum.toString()}-images`);
+    newForm.childNodes[3].childNodes[1].value='';
     imgFormContainer.append(newForm) //Insert the new form at the end of the list of forms
 
     totalForms.value = imgFormNum+1;
+}
+
+function upload_img(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            //input.before('<div class="col-sm-4"><img id="img_id" class="img-thumbnail" src="'+e.target.result+'" alt="your image" /></div>" />');
+            let image = document.createElement("img")
+            image.setAttribute("class", "img-thumbnail")
+            image.src = e.target.result
+            console.log(input.parentElement.parentElement.childNodes[1])
+            input.parentElement.parentElement.childNodes[1].innerHTML='';
+            input.parentElement.parentElement.childNodes[1].append(image);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+        //$(".addImg").click();
+    }
 }
