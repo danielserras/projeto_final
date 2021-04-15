440dbdc7221a05766ba9d5c1dcf4cab05da86a54
danielserras <dfserras@gmail.com>
2021-04-15 19:40:51 +0100

    preview images to upload in addListing

--- projeto_final_/projeto_final/settings.py
+++ projeto_final_/projeto_final/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '269b8371dfda.ngrok.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '7eaf382b6a72.ngrok.io']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
