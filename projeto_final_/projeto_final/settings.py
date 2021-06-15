f765d543f2aee0ad2baf66d22c3a42dfba089791
jRibeir0 <fc53331@alunos.fc.ul.pt>
2021-06-16 00:24:37 +0100

    cloud storage setup

--- projeto_final_/projeto_final/settings.py
+++ projeto_final_/projeto_final/settings.py
@@ -186,8 +186,8 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale') ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+#STATIC_URL = '/static/'
+#STATIC_ROOT = "/static/"
 
 from django.utils.translation import gettext_lazy as _
 
