a24f5072031014330e3756ceae33c18722a0caf2
jRibeir0 <fc53331@alunos.fc.ul.pt>
2021-06-16 00:45:48 +0100

    cloud storage setup

--- projeto_final_/projeto_final/settings.py
+++ projeto_final_/projeto_final/settings.py
@@ -21,15 +21,15 @@ STATICFILES_STORAGE = 'gcloud.GoogleCloudStaticFileStorage'
     
 GS_PROJECT_ID = 'dotted-clover-316000'
 GS_STATIC_BUCKET_NAME = 'unihouses_imgs'
-GS_MEDIA_BUCKET_NAME = 'unihouses_imgs1'
+#GS_MEDIA_BUCKET_NAME = 'unihouses_imgs'
 
 STATIC_URL = 'https://storage.googleapis.com/{}/'.format(GS_STATIC_BUCKET_NAME)
 STATIC_ROOT = "static/"
 
-MEDIA_URL = 'https://storage.googleapis.com/{}/'.format(GS_MEDIA_BUCKET_NAME)
-MEDIA_ROOT = "media/"
+#MEDIA_URL = 'https://storage.googleapis.com/{}/'.format(GS_MEDIA_BUCKET_NAME)
+#MEDIA_ROOT = "media/"
     
-UPLOAD_ROOT = 'media/uploads/'
+#UPLOAD_ROOT = 'media/uploads/'
     
 DOWNLOAD_ROOT = os.path.join(PROJECT_ROOT, "static/media/downloads")
 DOWNLOAD_URL = STATIC_URL + "media/downloads"
